c7e3140041316bde265502124342d612b0714d2b
Michael Kelly <mkelly@mozilla.com>
2012-04-24 17:10:51 -0700

    Add loader and graphic components.

--- flux/entity.js
+++ flux/entity.js
@@ -2,6 +2,7 @@ define(function(require) {
     function Entity(x, y) {
         this.x = x || 0;
         this.y = y || 0;
+        this.graphic = null;
     }
 
     Entity.prototype = {
@@ -10,7 +11,9 @@ define(function(require) {
         },
 
         render: function(ctx) {
-
+            if (this.graphic !== null) {
+                this.graphic.render(ctx, this.x, this.y);
+            }
         }
     };
 
