a20b084250c62734e418c06ffa0fc043c5728a40
Michael Kelly <mkelly@mozilla.com>
2012-05-09 20:55:54 -0700

    Add method for writing text via tiled graphics.

---
+++ flux/util.js
@@ -0,0 +1,25 @@
+define(function(require, exports) {
+    /**
+     * Collection of utility functions small enough to not warrant their own
+     * file.
+     */
+
+    /**
+     * Draws a string of text, using the given tiled graphic as the font.
+     *
+     * Tile numbers correspond to the char codes in the string.
+     */
+    exports.renderText = function(ctx, text, x, y, fontGraphic, maxWidth) {
+        var left = x;
+        for (var k = 0; k < text.length; k++) {
+            var code = text.charCodeAt(k);
+            fontGraphic.renderTile(ctx, code, x, y);
+
+            x += fontGraphic.tileWidth;
+            if (maxWidth !== undefined && (x - left) > maxWidth) {
+                x = left;
+                y += fontGraphic.tileHeight;
+            }
+        }
+    };
+});
